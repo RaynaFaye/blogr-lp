b03cd7d7db6c1c07d9aeafae60534059e92baa2f
Serena <serenafayelombardo@gmail.com>
2021-06-03 15:17:47 +0200

    Added functionality to open and close menu in mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+// Menu functionality for mobile
+const menuButton = document.querySelector('.navigation__mobile-menu-button');
+const menuButtonImage = menuButton.querySelector('img');
+const menu = document.querySelector('.navigation');
+const menuLinks = document.querySelectorAll('.menu__link__item');
+
+menuButton.addEventListener('click', () => {
+  if (menu.classList.contains('hidden')) {
+    menuButtonImage.src = 'images/icon-close.svg';
+    menuButton.setAttribute('aria-expanded', 'true');
+  } else {
+    menuButtonImage.src = 'images/icon-hamburger.svg';
+    menuButton.setAttribute('aria-expanded', 'false');
+  }
+  menu.classList.toggle('hidden');
+});
+
+window.addEventListener('keyup', () => {
+  if (!menu.classList.contains('hidden') && event.key === 'Escape') {
+    menu.classList.add('hidden');
+    menuButtonImage.src = 'images/icon-hamburger.svg';
+    menuButton.setAttribute('aria-expanded', 'false');
+  }
+});
+
+menuLinks.forEach((link) => {
+    link.addEventListener('focusout', (event) => {
+    if (event.relatedTarget.classList.contains('menu__link__item')) {
+      return;
+    } else {
+      menu.classList.add('hidden');
+      menuButtonImage.src = 'images/icon-hamburger.svg';
+      menuButton.setAttribute('aria-expanded', 'false');
+    }
+  });
+});
+
+// Show Dropdown menus when focus-within hasn't got browser support
 let dropdownMenu = document.querySelectorAll('.navigation__menu__item-title')
 
 try {
@@ -14,6 +52,7 @@ try {
     }
 }
 
+// Change of images and arrows for mobile view
 let sectThreeImage = document.querySelector('.sect-three__image-block__image')
 let sectFirstImage = document.querySelector('.sect-first__image-block__image')
 
@@ -21,3 +60,11 @@ if (window.screen.width < 768) {
     sectThreeImage.src = "images/illustration-laptop-mobile.svg"
     sectFirstImage.src = "images/illustration-editor-mobile.svg"
 }
+
+let arrowIcons = document.querySelectorAll('.navigation__menu__item-title span img')
+
+arrowIcons.forEach(arrowIcon => {
+    if (window.screen.width < 768) {
+        arrowIcon.src = "images/icon-arrow-dark.svg"
+    }
+})
